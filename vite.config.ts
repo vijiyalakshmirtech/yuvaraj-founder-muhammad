1ef593fadcaedda68a92fccc8cf028ee9c6a8a9a
viji <vijiyalakshmirtech@gmail.com>
2026-06-20 12:52:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    return {
-      server: {
+ return {
+
+  base: '/yuvaraj-founder-muhammad/',
+
+  server: {
         port: 3000,
         host: '0.0.0.0',
       },
