1f6017ebe4e7a6c4bcdd83f1c8f9ff627e182f27
viji <vijiyalakshmirtech@gmail.com>
2026-06-23 20:37:41 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,13 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import './index.css';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
+document.title = "YUVARAJ RAMALINGAM | Founder of SEYONIX";
+window.scrollTo(0, 0);
 root.render(
   <React.StrictMode>
     <App />
